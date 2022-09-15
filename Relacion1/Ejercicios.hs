e053bd494cf2a30e381c906e2c310f2a13d710f0
Javiki <javipatinador@gmail.com>
2022-09-15 12:43:38 +0200

    Ejercicio 1 terminado

--- Relacion1/Ejercicios.hs
+++ Relacion1/Ejercicios.hs
@@ -9,7 +9,7 @@
 -- Relación de Ejercicios 1. Ejercicios resueltos: ..........
 --
 -------------------------------------------------------------------------------
---import Test.QuickCheck
+import Test.QuickCheck
 
 --Ejercicio 1
 -- Tres enteros positivos x, y, z constituyen una terna pitagórica si x^2 +y^2 =z^2, es decir, si son los lados de un triángulo rectángulo.
@@ -28,6 +28,20 @@ esTerna x y z | x*x + y*y == z*z = True
 -- (8,6,10)
 -- Main> esTerna 8 6 10
 -- True
+
 terna :: Integer -> Integer -> (Integer, Integer, Integer)
 terna x y | x > y = (x*x - y*y, 2*x*y, x*x + y*y)
           | otherwise = error "x no es menor que y"
+
+-- c) Lee y entiende la siguiente propiedad, para comprobar que todas las ternas generadas por la función terna son pitagóricas:
+
+p_ternas x y = x>0 && y>0 && x>y ==> esTerna l1 l2 h
+    where
+        (l1,l2,h) = terna x y
+
+--d) Comprueba esta propiedad usando QuickCheck (recuerda importar Test.QuickCheck al principio de tu programa y copiar la propiedad en tu fichero). 
+-- Verás que la respuesta es parecida a:
+-- Main> quickCheck p_ternas
+-- *** Gave up! Passed only 62 tests
+-- lo que indica que, aunque sólo se generaron 62 casos de pruebas con las condiciones precisas, todos estos los casos pasaron la prueba.
+
