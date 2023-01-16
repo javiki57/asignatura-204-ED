fd413e13e47975c28a3d117415b60c9d271920e4
Javiki <javipatinador@gmail.com>
2023-01-16 16:46:02 +0100

    June2020 haskell

--- Finales/Febrero2020/code/haskell/src/DataStructures/Graph/FordFulkerson.hs
+++ Finales/Febrero2020/code/haskell/src/DataStructures/Graph/FordFulkerson.hs
@@ -14,23 +14,42 @@ import DataStructures.Graph.WeightedDiGraph
 import DataStructures.Graph.WeightedDiGraphBFT
 
 maxFlowPath :: Path (WDiEdge a Integer) -> Integer
-maxFlowPath [] = 0
-maxFlowPath path = minimum [ p | (E a p b) <- path]
+maxFlowPath path = minimum [ w | (E _ w _ ) <- path ]
 
 updateEdge ::(Eq a) => a -> a -> Integer -> [WDiEdge a Integer] -> [WDiEdge a Integer]
-updateEdge = undefined
+updateEdge x y p [] = [E x p y]
+updateEdge x y p ((E f w t):xs)
+                | pertenece && not sumacero = (E f (w+p) t):xs
+                | pertenece && sumacero     = xs
+                | otherwise                 = (E f w t) : (updateEdge x y p xs)
+          where
+            pertenece = x==f && y==t
+            sumacero = (w+p) == 0
+
 
 updateEdges :: (Eq a) => Path (WDiEdge a Integer) -> Integer -> [WDiEdge a Integer] -> [WDiEdge a Integer]
-updateEdges = undefined
+updateEdges path p edges = foldr (\(E f w t) acc -> updateEdge f t p acc) edges path
 
 addFlow :: (Eq a) => a -> a -> Integer -> [WDiEdge a Integer] -> [WDiEdge a Integer]
-addFlow = undefined
+addFlow x y p [] = [E x p y]
+addFlow x y p ((E f w t):sol)
+          | pertenece = (E f (w+p) t):sol
+          | contrario = sol
+          | contrario && menor = (E f (p-w) t):sol
+          | contrario && mayor = (E f (w-p) t):sol
+          | otherwise = addFlow x y p sol
+        where
+          pertenece = x==f && y==t
+          contrario = x==t && y==f && w==p
+          menor = w < p
+          mayor = w > p
+
 
 addFlows :: (Eq a) => Path (WDiEdge a Integer) -> Integer -> [WDiEdge a Integer] -> [WDiEdge a Integer]
-addFlows = undefined
+addFlows path p sol = foldr (\(E f w t) -> addFlow f t p ) sol path
 
 fordFulkerson :: (Ord a) => (WeightedDiGraph a Integer) -> a -> a -> [WDiEdge a Integer]
-fordFulkerson = undefined
+fordFulkerson g src dst = undefined
 
 maxFlow :: (Ord a) => [WDiEdge a Integer] -> a -> Integer
 maxFlow = undefined
@@ -52,3 +71,4 @@ sourcesAndSinks = undefined
 
 unifySourceAndSink :: (Eq a) => WeightedDiGraph a Integer -> a -> a -> WeightedDiGraph a Integer
 unifySourceAndSink = undefined
+
