2b0abc93a68b7f8d7f4fdb9ff5853af24efc8d9a
Javiki <javipatinador@gmail.com>
2023-01-18 16:43:19 +0100

    Practica 9 added

--- Practica9/Haskell/DataStructures/Graph/TopologicalSortingDic.hs
+++ Practica9/Haskell/DataStructures/Graph/TopologicalSortingDic.hs
@@ -1,15 +1,18 @@
 -------------------------------------------------------------------------------
--- Estructuras de Datos
--- Práctica 9 - Orden topológico de un digrafo (sin clonar el grafo)
+-- Topological Sorting of a Graph
+--
+-- Data Structures. Grado en Informática. UMA.
+-- Pepe Gallardo, 2012
 -------------------------------------------------------------------------------
 
-module TopologicalSortingDic
+module DataStructures.Graph.TopologicalSortingDic
   ( topSorting
   , topSortings
   ) where
 
-import           DataStructures.Dictionary.AVLDictionary as D
-import           DataStructures.Graph.DiGraph
+import DataStructures.Graph.DiGraph
+import DataStructures.Dictionary.AVLDictionary as D
+import Data.List((\\))
 
 -- Decrement 1 the value associated to key v
 decrementValue :: (Num b, Ord a) => a -> Dictionary a b -> Dictionary a b
@@ -21,26 +24,27 @@ decrementValue v dic = D.insert v (u-1) dic
 removeKeys :: (Ord a) => [a] -> Dictionary a b -> Dictionary a b
 removeKeys keys dic = foldr D.delete dic keys
 
--- compute topological order
+
 topSorting :: (Ord a) => DiGraph a -> [a]
 topSorting g  = aux initPenPred
  where
-  initPenPred = undefined
+  initPenPred = 
   aux pendingPred
-    | D.isEmpty pendingPred = undefined
-    | null srcs             = error "DiGraph is cyclic"
-    | otherwise             = undefined
+    | D.isEmpty pendingPred = []
+    | null srcs    = error "DiGraph is cyclic"
+    | otherwise  = undefined
     where
-      srcs = undefined::[a] -- list of vertices with 0 pending predecessors
+      srcs = undefined -- list of vertices with inDegree 0
 
--- compute topological order in nested collection
+-- Include information of parallel tasks
 topSortings :: (Ord a) => DiGraph a -> [[a]]
 topSortings g  = aux initPenPred
   where
     initPenPred = undefined
     aux pendingPred
-      | D.isEmpty pendingPred = undefined
-      | null srcs             = error "DiGraph is cyclic"
-      | otherwise             = undefined
+      | D.isEmpty pendingPred = []
+      | null srcs  = error "DiGraph is cyclic"
+      | otherwise  = undefined
       where
-        srcs = undefined::[a] -- list of vertices with 0 pending predecessors
+        srcs = undefined -- list of vertices with inDegree 0
+       
