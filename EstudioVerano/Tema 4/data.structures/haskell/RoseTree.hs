b453d8f8ab665099eba5e86b95d45c593f697652
Javiki <javipatinador@gmail.com>
2023-08-07 12:53:27 +0200

    RoseTree done

--- EstudioVerano/Tema 4/data.structures/haskell/RoseTree.hs
+++ EstudioVerano/Tema 4/data.structures/haskell/RoseTree.hs
@@ -25,22 +25,31 @@ gtree1 =
 -- >>> sumT gtree1
 -- 28
 sumT :: Num a => Tree a -> a
-sumT = undefined
+sumT Empty = 0
+sumT (Node a s) = a + sum (map sumT s)
 
 -- | altura (número de niveles) de un árbol genérico
 -- >>> heightT gtree1
 -- 3
 heightT :: Tree a -> Int
-heightT = undefined
+heightT Empty      = 0
+heightT (Node a s) = case s of
+        [] -> 1
+        xs -> 1 + maximum (map heightT xs)
 
 -- | frontera (conjunto de nodos hoja) de un árbol genérico
 -- >>> borderT gtree1
 -- [4,5,6,7]
 borderT :: Tree a -> [a]
-borderT = undefined
+borderT Empty       = []
+borderT (Node a []) = [a]
+borderT (Node a s)  = concatMap borderT s
 
 -- | recorrido de un árbol genérico
 -- >>> flattenT gtree1
 -- [1,2,4,5,6,3,7]
 flattenT :: Tree a -> [a]
-flattenT = undefined
+flattenT Empty = []
+flattenT (Node a s) = case s of
+        [] -> [a]
+        xs -> a : (concatMap flattenT xs) 
