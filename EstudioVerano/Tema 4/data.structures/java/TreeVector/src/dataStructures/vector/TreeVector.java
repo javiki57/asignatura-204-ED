ae7c19af3b589f31bd924d7efdcc8220fa8ef479
Javiki <javipatinador@gmail.com>
2023-08-09 10:50:02 +0200

    TreeVector.java done

--- EstudioVerano/Tema 4/data.structures/java/TreeVector/src/dataStructures/vector/TreeVector.java
+++ EstudioVerano/Tema 4/data.structures/java/TreeVector/src/dataStructures/vector/TreeVector.java
@@ -6,7 +6,10 @@
 
 package dataStructures.vector;
 
+import dataStructures.list.ArrayList;
 import dataStructures.list.List;
+
+import java.util.Iterator;
 
 public class TreeVector<T> {
 
@@ -30,19 +33,20 @@ public class TreeVector<T> {
 
         @Override
         public E get(int i) {
-            // TODO
-            return null;
+
+            return value;
         }
 
         @Override
         public void set(int i, E x) {
-            // TODO
+            value = x;
         }
 
         @Override
         public List<E> toList() {
-            // TODO
-            return null;
+            List<E> lista = new ArrayList<>();
+            lista.append(value);
+            return lista;
         }
     }
 
@@ -57,57 +61,105 @@ public class TreeVector<T> {
 
         @Override
         public E get(int i) {
-            // TODO
-            return null;
+            if(i % 2 == 0){
+                return left.get(i/2);
+            }else{
+                return right.get(i/2);
+            }
         }
 
         @Override
         public void set(int i, E x) {
-            // TODO
+            if(i % 2 == 0){
+                left.set(i/2, x);
+            }else{
+                right.set(i/2,x);
+            }
         }
 
         @Override
         public List<E> toList() {
-            // TODO
-            return null;
+
+            return intercalate(left.toList(),right.toList());
         }
     }
 
     public TreeVector(int n, T value) {
-        // TODO
+
+        if(n<0) throw new VectorException("Exponente negativo");
+
+        size = (int) Math.pow(2,n);
+        root = tree (n, value);
+    }
+
+    private Tree<T> tree(int n, T value){
+        if(n == 0){
+            return new Leaf<>(value);
+
+        }else{
+            Tree<T> left = tree(n-1, value);
+            Tree<T> right = tree(n-1, value);
+            return new Node<>(left,right);
+
+        }
     }
 
     public int size() {
-        // TODO
-        return 0;
+
+        return size;
     }
 
     public T get(int i) {
-        // TODO
-        return null;
+        if(i >= size || i < 0) throw new VectorException("Error, posición del vector errónea.");
+
+
+        return root.get(i);
     }
 
     public void set(int i, T x) {
-        // TODO
+        if(i >= size || i < 0) throw new VectorException("Error, posición del vector errónea.");
+        else{
+            root.set(i,x);
+        }
     }
 
     public List<T> toList() {
-        // TODO
-        return null;
+        List<T> lista = root.toList();
+
+        return lista;
     }
 
     protected static <E> List<E> intercalate(List<E> xs, List<E> ys) {
-        // TODO
-        return null;
+        List<E> lista = new ArrayList<>();
+        int i = 0;
+
+        Iterator<E> x = xs.iterator();
+        Iterator<E> y = ys.iterator();
+        while(x.hasNext() && y.hasNext()){
+            lista.append(x.next());
+            lista.append(y.next());
+        }
+
+        return lista;
     }
 
     static protected boolean isPowerOfTwo(int n) {
-        // TODO
-        return false;
+        int exp = 0;
+        while(Math.pow(2,exp) != n && Math.pow(2, exp) <=n){
+            exp++;
+        }
+
+        return Math.pow(2, exp) > n;
     }
 
+    //Firulo este último método.
     public static <E> TreeVector<E> fromList(List<E> l) {
-        // TODO
-        return null;
+        if(!isPowerOfTwo(l.size())){
+            throw new VectorException("La longitud de la lista no es potencia de 2");
+        }
+
+        return new TreeVector<>(l.size(), l.get(0));
     }
+
+
 }
