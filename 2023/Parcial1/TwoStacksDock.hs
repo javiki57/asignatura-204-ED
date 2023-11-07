e31558b81a8c9c01bdc66cefb9dac85ed673d4fc
Javiki <javipatinador@gmail.com>
2023-11-07 13:49:36 +0100

    Examenes hechos

---
+++ 2023/Parcial1/TwoStacksDock.hs
@@ -0,0 +1,213 @@
+-- =====================================================
+-- ============ Puntuación máxima 1.25 =================
+-- =====================================================
+-- Un Dock es una secuencia de elementos en el que hay uno destacado (sign)
+-- Las operaciones disponibles para un dock son:
+-- Borrar el elemento destacado, añadir un nuevo elemento delante o detrás del destacado,
+-- cambiar el elemento destacado al elemento anterior o al siguiente,
+-- saber si la secuencia es vacia o si el destacado es el primero o el último
+-- y crear un dock a partir de una lista
+
+module DataStructures.Dock.TwoStacksDock  (
+    empty,           -- :: Dock a
+    isEmpty,         -- :: Dock a -> Bool
+    sign,            -- :: Dock a ->  a
+    isFirst,         -- :: Dock a -> Bool
+    isLast,          -- :: Dock a -> Bool
+    left,            -- :: Dock a ->  Dock a
+    right,           -- :: Dock a -> Dock a
+    delete,          -- :: Dock a ->  Dock a
+    insertl,         -- :: a  ->  Dock a ->  Dock a
+    insertr,         -- :: a  ->  Dock a ->  Dock a
+    listToDock       -- :: [a] -> Dock a
+) where
+
+import Data.List(intercalate)
+import qualified DataStructures.Stack.LinearStack as S
+import Test.QuickCheck
+ 
+-- Vamos a implementar el dock con dos pilas:
+-- En la primera pila estarán los elementos anteriores al destacado siendo 
+-- el mas cercano al destacado el que está en la cima.
+-- En la segunda pila, el destacado será la cima e seguirán el resto siendo 
+-- el de la derecha del destacado el que está el siguiente en la pila.
+-- INVARIANTES:
+-- Siempre hay un elemento destacado (salvo en el dock vacío).
+-- Siempre mantendremos el elemento destacado en la cima de la segunda pila.
+
+data Dock a =  D (S.Stack a) (S.Stack a) deriving Eq
+-- Ejemplo. Si tenemos el dock: 1 2 3 <4> 5 donde el elemento destacado es el 4, se presentará por el dato
+-- D S.push 3 $ S.push 2 $ S.push 1 S.empty) (S.push 4 $ S.push 5 S.empty)
+-- Su show será TwoStackDock(1,2,3,<4>,5)
+-- y las pilas se verán asi:
+--       3           
+--       2           4 <- el destacado siempre en la cima de la segunda pila
+--       1           5
+--     -----       -----
+-- NOTA: Todas las operaciones con pilas están cualificadas con S: S.Stack, S.push, S.pop, etc.
+sample1 = D (S.push 3 $ S.push 2 $ S.push 1 S.empty) (S.push 4 $ S.push 5 S.empty)
+
+-- ===========================================================
+-- Ejercicio 1 (0.05 ptos.)
+-- Crea una dock vacío
+empty :: Dock a 
+empty = D (S.empty) (S.empty)
+
+-- ===========================================================
+-- Ejercicio 2 (0.05 ptos.)
+-- Determina si un dock está vacío
+isEmpty :: Dock a -> Bool
+isEmpty (D s1 s2) = S.isEmpty s1 && S.isEmpty s2
+
+-- ===========================================================
+-- Ejercicio 3 (0.10 ptos.)
+-- Devuelve el elemento destacado. Error si está vacío
+sign  :: Dock a ->  a
+sign s@(D s1 s2)
+        | isEmpty s = error "Pilas vacias"
+        | otherwise = S.top s2
+    
+{-
+Prelude (Dock.hs)> sign sample1
+4
+-}
+
+-- ===========================================================
+-- Ejercicio 4 (0.10 ptos.)
+-- Devuelve cierto si el destacado es el primero (o está vacía)
+isFirst :: Dock a -> Bool
+isFirst s@(D s1 s2) = S.isEmpty s1 
+
+{-
+Prelude (Dock.hs)> isFirst sample1
+False
+-}
+-- ===========================================================
+-- Ejercicio 5 (0.10 ptos.)
+-- Devuelve cierto si el destacado es el último (o está vacía)
+isLast :: Dock a -> Bool
+isLast s@(D s1 s2) = S.isEmpty (S.pop s2)
+
+{-
+Prelude (Dock.hs)> isLast sample1
+False
+-}
+
+-- ===========================================================
+-- Ejercicio 6 (0.20 ptos.)
+-- Cambia el elemento destacado que pasa a ser el de la izquierda.
+-- Si está vacío o el destacado es el primero lo deja igual
+left :: Dock a ->  Dock a
+left s@(D s1 s2)
+    | isFirst s = s
+    | otherwise = D (S.pop s1) (S.push (S.top s1) s2)
+--Hacemos un pop de la primera pila y hacemos un push 
+--del que quitamos de la primera fila(top) en la segunda fila
+
+{-
+Prelude (Dock.hs)> left sample1
+TwoStackDock(1,2,<3>,4,5)
+Prelude (Dock.hs)> left $ left sample1
+TwoStackDock(1,<2>,3,4,5)
+Prelude (Dock.hs)> left $ left $ left sample1
+TwoStackDock(<1>,2,3,4,5)
+Prelude (Dock.hs)> left $ left $ left $ left sample1
+TwoStackDock(<1>,2,3,4,5)
+Prelude (Dock.hs)> isFirst $ left $ left $ left sample1
+True
+-}
+-- ===========================================================
+-- Ejercicio 7 (0.20 ptos.)
+-- El elemento destacado pasa a ser el de la derecha.
+-- Si está vacío o el destacado es el último  lo deja igual
+right :: Dock a ->  Dock a
+right s@(D s1 s2 )
+        | isLast s  = s
+        | otherwise = D (S.push (S.top s2) s1) (S.pop s2)
+        
+
+{-
+Prelude (Dock.hs)> right sample1
+TwoStackDock(1,2,3,4,<5>)
+Prelude (Dock.hs)> right $ right sample1
+TwoStackDock(1,2,3,4,<5>)
+Prelude (Dock.hs)> isLast $ right sample1
+True
+-}
+-- ===========================================================
+-- Ejercicio 8 (0.20 ptos.)
+-- Elimina el objeto destacado. El destacado pasa a ser el siguiente.
+-- Si no hay siguiente pasa a ser el anterior.
+-- Si queda vacía no hay destacado.
+-- Error si está vacía
+delete :: Dock a ->  Dock a
+delete s@(D s1 s2)
+        | isEmpty s = error "Está vacía quillo"
+        | isLast s  = if S.isEmpty s1 then empty else D (S.pop s1) (S.push (S.top s1) (S.pop s2))
+        | otherwise = D s1 (S.pop s2)
+
+{-
+Prelude (Dock.hs)> delete sample1
+TwoStackDock(1,2,3,<5>)
+Prelude (Dock.hs)> delete $ delete sample1
+TwoStackDock(1,2,<3>)
+Prelude (Dock.hs)> delete $ delete $ delete sample1
+TwoStackDock(1,<2>)
+Prelude (Dock.hs)> delete $ delete $ delete $ delete sample1
+TwoStackDock(<1>)
+Prelude (Dock.hs)> delete $ delete $ delete $ delete $ delete sample1
+TwoStackDock()
+-}
+-- ===========================================================
+-- Ejercicio 9 (0.10 ptos.)
+-- inserta el elemento a la izquierda del destacado y este elemento pasa a ser el destacado
+insertl :: a  ->  Dock a ->  Dock a
+insertl x (D s1 s2) = D (s1) (S.push x s2)
+-- ===========================================================
+-- Ejercicio 10 (0.10 ptos.)
+-- inserta el elemento a la derecha del destacado y este elemento pasa a ser el destacado
+insertr :: a  ->  Dock a ->  Dock a
+insertr x s@(D s1 s2) = D (S.push (S.top s2) s1) (S.push x (S.pop s2))
+
+
+{-
+Prelude (Dock.hs)> insertl 8 sample1
+TwoStackDock(1,2,3,<8>,4,5)
+Prelude (Dock.hs)> insertr 9 $ insertl 8 sample1
+TwoStackDock(1,2,3,8,<9>,4,5)
+Prelude (Dock.hs)> insertr 9 $ right sample1
+TwoStackDock(1,2,3,4,5,<9>)
+Prelude (Dock.hs)> insertl 5 empty
+TwoStackDock(<5>)
+-}
+-- ===========================================================
+-- Ejercicio 11 (0.05 ptos.)
+-- genera un dock con los elementos de la lista. El destacado será el primero de la lista
+listToDock :: [a] -> Dock a
+listToDock d = foldr (insertl) empty d
+            
+{-
+Prelude (Dock.hs)> listToDock [1..5]
+TwoStackDock(<1>,2,3,4,5)
+-}
+-- =============================================================================
+-- ========================= NO TOCAR DE AQUÍ PARA ABAJO =======================
+instance (Show a) => Show (Dock a) where
+    show d@(D si sd) | isEmpty d = "TwoStackDock()"
+    show (D si sd) = "TwoStackDock("++(intercalate "," (reverse (aux si))) ++ 
+                        (if ei then "<" else ",<") ++ y ++(if ed then ">" else ">,") ++ (intercalate "," ys)++ ")"
+        where 
+            aux s
+                    | S.isEmpty s = []
+                    | otherwise = show x : aux s'
+                where
+                    x = S.top s
+                    s' = S.pop s
+            (y:ys) = aux sd
+            ei = S.isEmpty si
+            ed = S.isEmpty (S.pop sd) 
+
+instance Arbitrary a => Arbitrary (Dock a) where
+    arbitrary = do  
+            xs <- listOf arbitrary
+            return (foldr insertl empty xs) 
---
+++ 2023/Parcial1/polynomial-Plantilla.hs
@@ -0,0 +1,145 @@
+------------------------------------------------------------------
+--Parcial 2022 Haskell
+--Polynomial
+------------------------------------------------------------------
+module Polynomial(
+    Pol
+    ,empty
+    ,grade
+    ,coeff
+    ,insert
+    ,remove
+    ,list2Pol
+    ,sumP
+    ,foldPol
+    ,eval
+)where
+    --propiedad p1_suma quickCheck
+
+import Test.QuickCheck
+
+type Grade = Int
+type Coefficient = Integer
+
+data Pol = Nil | P Grade Coefficient (Pol) deriving Show
+--un polinomio se almacena como una secuencia de terminos ordenados de mayor a menor grado
+--Invariantes : Solo se almacenan los terminos con coeficiente distinto de 0
+--      Todos los terminos tineen diferente grado
+
+--Por ejemplo
+--p1 = x^3 + 2x^2 + 8 se representa
+p1 = P 3 1 (P 2 2 (P 0 8 Nil))
+--p2 = x^5 + 8x
+p2 = P 5 2 (P 1 8 Nil)
+
+--Nil representa un polinomio sin terminos (vacio)
+
+--Ejercicio 1: Devuelve un polinomio vacio
+empty :: Pol
+empty = Nil
+
+--Ejercicio 2: Devuelve el grado maximo del polinomio
+grade:: Pol -> Grade
+grade (P g c p) = g
+
+--Ejercicio 3: Devuelve el coeficiente del termino de grado g
+coeff :: Grade -> Pol -> Coefficient
+coeff g (P n c p)
+        | g == n    = c
+        | g < n     = coeff g p
+        | otherwise = error "El polinomio no tiene ese grado"
+
+--Ejercicio 4: Añade el termino de grado g con coeficiente c al polinomio.
+--Si el polinomio ya tenia un termino de ese grado, actualiza el coeficiente,
+-- es decir reemplaza el antiguo coeficiente por el nuevo
+insert:: Grade -> Coefficient -> Pol -> Pol
+insert g 0 p = p
+insert g c Nil = P g c Nil
+insert g c s@(P n c2 p)
+        | g == n    = (P g c p)
+        | g < n     = (P n c2 (insert g c p))
+        | otherwise = (P g c s)
+
+insert' :: Grade -> Coefficient -> Pol -> Pol
+insert' g 0 p = p
+insert' g c Nil = P g c Nil
+insert' g c s@(P g2 c2 p)
+        | g == g2   = P g (c+c2) p
+        | g < g2    = P g2 c2 (insert g c p)
+        | otherwise = s
+
+
+--Ejercicio 5: Elimina el termino g del polinomio
+--Si el polinomio no tenia termino de grado g no hace nada
+remove :: Grade -> Pol -> Pol
+remove g Nil = Nil
+remove g (P n c p)
+        | g == n    = p
+        | g < n     = (P n c (remove g p))
+        | otherwise = P n c p
+
+
+--Ejercicio 6: Dada una lista con los coeficientes de los terminos de un polinomio,
+--donde la posicion indica el grado, devuelve el Pol correspondiente
+--Si la lista tinen un coeficiente 0 ese termino no se almacena en la lista.
+--Por ejemplo, (x^3 + 2x^2 +8) se representa con la lista [8,0,2,1]
+--y usando list2Pol se obtiene el polinomio P 3 1 (P 2 2 (P 0 8 Nil))
+list2Pol :: [Integer] -> Pol
+list2Pol lista = foldr (\(c,g) p -> insert g c p) empty $ zip lista [0..]
+
+
+
+--Ejercicio 7: suma dos polinomios que pueden tener diferente grado
+sumP :: Pol -> Pol -> Pol
+sumP Nil p2 = p2
+sumP p1 Nil = p1
+sumP (P g1 c1 p1) (P g2 c2 p2)
+        | g1 == g2  = insert g1 (c1+c2) (sumP p1 p2)
+        | g1 < g2   = (P g2 c2 (sumP (P g1 c1 p1) p2))
+        | otherwise = (P g1 c1 (sumP p1 (P g2 c2 p2)))
+
+--Ejercicio 8: plegado a la derecha de polinomios
+foldPol :: (Grade -> Coefficient -> b -> b) -> b -> Pol -> b
+foldPol f z Nil = z
+foldPol f z (P g c p) = f g c (foldPol f z p)
+
+
+--Ejercicio 9: evaluacion del polinomio
+--por ejemplo, si tenemos el polinomio x^2 + 1 representado como
+--p1 = P 2 1 (P 0 1 Nil) eval 3 p1 devuelve el resultado de evaluar el polinomio
+--en 3 (3^2 + 1 = 10)
+eval:: Integer -> Pol -> Integer
+eval n Nil = 0
+eval n (P g c p) = c * (n^g) + (eval n p)
+
+
+--Ejercicio 10 : Establece una propiedad quickCheck que compruebe que la suma de
+--dos polinomios p1 y p2 tiene el mismo grado que el polinomio de mayor grado
+
+p1_suma :: Pol -> Pol -> Bool
+p1_suma p1 p2 =
+  let sumP1P2 = sumP p1 p2  -- Calcula la suma de p1 y p2
+      maxGradeP1 = grade p1  -- Grado de p1
+      maxGradeP2 = grade p2  -- Grado de p2
+      maxGradeSum = grade sumP1P2  -- Grado de la suma
+  in maxGradeSum == max maxGradeP1 maxGradeP2
+
+
+
+
+
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+---------------------NO TOCAR DESDE AQUI---------------------------------
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+completa :: (Int, Integer) -> (Pol, Bool) -> (Pol, Bool)
+completa (g,c) (p, b)
+    |b = (insert g c p, False)
+    |otherwise = (remove g p, False)
+
+instance Arbitrary Pol where
+    arbitrary = do
+        gs <- listOf (arbitrary:: Gen Int)
+        cs <- listOf (arbitrary:: Gen Integer)
+        return (fst (foldr completa (Nil, True) (map(\(g,c) -> (abs g, c)) $ filter (\(g,c) -> c/=0)(zip gs cs))))
