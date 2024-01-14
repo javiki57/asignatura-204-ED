ed425f1dfa485795c68d4c9f92667204a4015e85
Javiki <javipatinador@gmail.com>
2024-01-14 14:50:03 +0100

    Haskell Septiembre 2023 hecho

--- 2023-24/Finales/2023-Septiembre/Solución/haskell/SparseMatrix.hs
+++ 2023-24/Finales/2023-Septiembre/Solución/haskell/SparseMatrix.hs
@@ -17,40 +17,83 @@ data Index = Idx Int Int deriving (Eq, Ord, Show)
 data SparseMatrix = SM Int Int (D.Dictionary Index Int) deriving Show 
 
 sparseMatrix :: Int -> Int -> SparseMatrix
-sparseMatrix  = undefined -- to complete
+sparseMatrix f c 
+    | f > 0 && c > 0 = SM f c D.empty
+    | otherwise      = error "Dimensiones no válidas"
 
 -- | = Exercise b - value
 value :: SparseMatrix -> Index -> Int
-value  = undefined -- to complete
+value (SM f c dic) idx = case D.valueOf idx dic of
+    Nothing -> 0
+    Just x  -> x
 
 -- | = Exercise c - update
 update :: SparseMatrix -> Index -> Int -> SparseMatrix
-update  = undefined -- to complete
+update (SM f c dic) idx v
+        | v == 0    = SM f c (D.delete idx dic)
+        | otherwise = SM f c (D.insert idx v dic)
 
 -- | = Exercise d - index
 index :: SparseMatrix -> Int -> Int -> Index
-index  = undefined -- to complete
+index (SM f c dic) fil col
+        | fil <= f && col <= c && fil >= 0 && col >= 0 = Idx fil col
+        | otherwise                                    = error "Coordenadas no válidas"
 
 -- | = Exercise e - set
 set :: SparseMatrix -> Int -> Int -> Int -> SparseMatrix
-set  = undefined -- to complete
+set sm fil col v = update sm (index sm fil col) v
 
 -- | = Exercise f - get
 get :: SparseMatrix -> Int -> Int -> Int
-get  = undefined -- to complete
+get sm fil col = value sm (index sm fil col)
 
 -- | = Exercise g - add
 add :: SparseMatrix -> SparseMatrix -> SparseMatrix
-add  = undefined -- to complete
+add (SM f1 c1 dic1) (SM f2 c2 dic2)
+        | f1 /= f2 && c1 /= c2 = error "Dimensiones diferentes"
+        | otherwise = suma (SM f1 c1 dic1) (D.keysValues dic2)
+            where
+                suma sm@(SM f c dic1) [] = sm
+                suma sm@(SM f c dic) ((x,y):xs)
+                        | not(D.isDefinedAt x dic) = suma (update sm x y) xs
+                        | otherwise                = suma (update sm x (y + value sm x)) xs
+                            
+                                
+
 
 -- | = Exercise h - transpose
 transpose :: SparseMatrix -> SparseMatrix
-transpose  = undefined -- to complete
+transpose (SM f c dic) = let sm = sparseMatrix c f in actualizar sm (D.keysValues dic)
+    where
+        actualizar sm [] = sm
+        actualizar sm@(SM f c dic) (((Idx fil col),b):xs) = actualizar (update sm (index sm col fil) b) xs
+
+
+
+--SM 2 2 AVLDictionary(Idx 0 2->10,Idx 1 0->20)
+--SM 2 2 AVLDictionary(Idx 0 1->20,Idx 2 0->10)
+
+{-
+0 0 10 0
+20 0 0 0
+0 0 0 0
+
+0 20 0 0
+0  0 0 0
+10 0 0 0
+ 0 0 0 0
+-}
+
 
 -- | = Exercise i - fromList
 -- Complexity of fromList: ???? -- to complete
 fromList :: Int -> Int -> [Int] -> SparseMatrix
-fromList  = undefined -- to complete
+fromList f c ls
+    | mod (length ls) 3 /= 0 = error "La lista no es múltiplo de 3"
+    | otherwise              = crear (sparseMatrix f c) ls
+        where
+            crear sm []         = sm
+            crear sm (x:y:z:xs) = crear (update sm (index sm x y) z) xs
 
 
 
