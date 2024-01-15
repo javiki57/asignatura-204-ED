435b570d1b74451a7efe76e672a518aa40393df7
Javiki <javipatinador@gmail.com>
2024-01-15 12:02:13 +0100

    Haskell Enero 2016 hecho

--- 2023-24/Finales/2016-Febrero/Solución/haskell/DataStructures/Graph/EulerianCycle.hs
+++ 2023-24/Finales/2016-Febrero/Solución/haskell/DataStructures/Graph/EulerianCycle.hs
@@ -12,24 +12,51 @@ import Data.List
 
 --H.1)
 isEulerian :: Eq a => Graph a -> Bool
-isEulerian g = undefined
+isEulerian g = aux (vertices g)
+    where
+        aux [] = True
+        aux (x:xs) = even (degree g x) && aux xs
 
 -- H.2)
 remove :: (Eq a) => Graph a -> (a,a) -> Graph a
-remove g (v,u) = undefined
+remove g (v, u) = aux (deleteEdge g (v, u)) (vertices g)
+  where
+    aux grafo [] = grafo
+    aux grafo (x:xs)
+      | degree grafo x == 0 = aux (deleteVertex grafo x) xs
+      | otherwise           = aux grafo xs
 
 -- H.3)
 extractCycle :: (Eq a) => Graph a -> a -> (Graph a, Path a)
-extractCycle g v0 = undefined
+extractCycle g v0 = aux g v0 [v0]
+    where
+        aux g v xs
+            | u == v0 = (g', u:xs)
+            | otherwise = aux g' u (u:xs)
+                where
+                    g' = remove g (v,u)
+                    u = head (successors g v)
 
 -- H.4)
 connectCycles :: (Eq a) => Path a -> Path a -> Path a
-connectCycles xs (y:ys)  = undefined
+connectCycles [] ys     = ys
+connectCycles (x:xs) (y:ys)
+        | x == y    = (y:ys) ++ xs
+        | otherwise = x : connectCycles xs (y:ys)
 
 -- H.5)
 vertexInCommon :: Eq a => Graph a -> Path a -> a
-vertexInCommon g cycle = undefined
+vertexInCommon g xs = head $ filter (\v -> v `elem` xs) (vertices g)
 
 -- H.6) 
 eulerianCycle :: Eq a => Graph a -> Path a
-eulerianCycle g = undefined
+eulerianCycle g
+    | not (isEulerian g) = error "Error: Grafo No Euleriano"
+    | otherwise = aux g (head (vertices g)) []
+        where
+            aux g v xs
+                | isEmpty g = xs
+                | otherwise = aux g' (last xs') xs'
+                    where
+                        (g', cicloParcial) = extractCycle g v
+                        xs' = connectCycles xs cicloParcial
